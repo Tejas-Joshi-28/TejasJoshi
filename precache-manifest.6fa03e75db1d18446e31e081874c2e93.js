a8de9822995b9ad82b2ae0cf6fbdfc513e2ec013
TejasJoshi-web <tejas.22010508@viit.ac.in>
2023-04-16 13:00:59 +0530

    Updates

---
+++ precache-manifest.6fa03e75db1d18446e31e081874c2e93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "45a6c93e5d06c131a1b1",
+    "url": "/TejasJoshi/static/css/main.d94f3429.chunk.css"
+  },
+  {
+    "revision": "45a6c93e5d06c131a1b1",
+    "url": "/TejasJoshi/static/js/main.45a6c93e.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/TejasJoshi/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "58d1f7c4a128c4a09709",
+    "url": "/TejasJoshi/static/js/2.58d1f7c4.chunk.js"
+  },
+  {
+    "revision": "5c12e199b073e968135f",
+    "url": "/TejasJoshi/static/js/runtime~main.5c12e199.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TejasJoshi/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "0c8c71661a8b8a8f7e4ae3287029a72c",
+    "url": "/TejasJoshi/index.html"
+  }
+];
